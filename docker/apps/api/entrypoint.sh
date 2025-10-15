59c1f1913c7f803f1e534fccfea05fdc0745d303
noah-caiooliveira <caio@noahomni.com.br>
2025-10-15 09:04:58 -0300

    Allow overriding API entrypoint commands

--- docker/apps/api/entrypoint.sh
+++ docker/apps/api/entrypoint.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env sh
 set -e
+
+if [ "$#" -gt 0 ]; then
+  exec "$@"
+fi
+
 cd /app/apps/api
-node ./node_modules/.bin/prisma generate --schema ../prisma/schema.prisma
-node ./node_modules/.bin/prisma migrate deploy --schema ../prisma/schema.prisma
+node ./node_modules/.bin/prisma generate --schema ./prisma/schema.prisma
+node ./node_modules/.bin/prisma migrate deploy --schema ./prisma/schema.prisma
 exec node dist/main.js
